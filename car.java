1078b8e3310450908107ec0d3a99f882e63d2a4f
Chanatun Kohsuwan <chanatun.kohsuwan@gmail.com>
2025-10-29 22:29:09 -0400

    Add files via upload

---
+++ car.java
@@ -0,0 +1,16 @@
+public class car {
+
+public static String companyName = "trashCo";
+private String modelName;
+private double horsePower;
+private int numOfSeats;
+
+public void driveForward(int Speed){
+    System.out.println("Speed increased by" + Speed);
+}
+public void refuelTank(double percent){
+    System.out.println("Tank refueld by "+percent+"%");
+}
+
+
+}
---
+++ frcHomework2.java
@@ -0,0 +1,10 @@
+public class frcHomework2 {
+    public static void main(String[] args) {
+        car cool = new car();
+
+        System.out.println("Company: " + car.companyName);
+        cool.driveForward(10);
+        cool.refuelTank(50.0);
+    }
+}
+    
